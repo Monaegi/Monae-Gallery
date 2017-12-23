96ce6b89250e03e30990025e1a208c74bf6b6cb6
Gihong Kim <gyong1211@gmail.com>
2017-12-23 16:39:32 +0900

    Add JWT token 생성, 갱신 url 추가

--- django_app/config/urls.py
+++ django_app/config/urls.py
@@ -19,4 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^rent/', include('rent.urls')),
+    url(r'^member/', include('member.urls')),
 ]
