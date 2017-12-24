f889102e9906f929aeb79cf12a3e3c6ad64af0d2
bbungsang <821heejin@naver.com>
2017-12-24 12:26:30 +0900

    conflict 해결, CalendarView 월~일 -> 일~월 수정

---
+++ django_app/rent/views/scheduler.py
@@ -0,0 +1,16 @@
+import calendar
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+
+class CalendarView(APIView):
+    def get(self, request, year, month):
+        print(year, month)
+        cal = calendar.TextCalendar()
+        print(year, month)
+        days = [0] + list(cal.itermonthdays(int(year), int(month)))
+        week_list = [days[i:i+7] for i in range(0, len(days), 7)]
+        if week_list[0][6] == 0:
+            return Response({"calendar": week_list[1:]})
+        return Response({"calendar": week_list})
