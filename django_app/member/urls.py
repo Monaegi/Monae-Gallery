921100c7dcf76aad79511beaecdce6c5872b93ac
Gihong Kim <gyong1211@gmail.com>
2018-01-07 00:39:45 +0900

    Modify - JWT Authentication 제거, DRF Token 방식 인증 사용 예정

--- django_app/member/urls.py
+++ django_app/member/urls.py
@@ -1,7 +1,5 @@
 from django.conf.urls import url
-from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token
+
 
 urlpatterns = [
-    url('^token-auth/$', obtain_jwt_token, name="get_token"),
-    url('^token-refresh/$', refresh_jwt_token, name="refresh_token"),
 ]
