85a0ec6e61222630da6d66799bc5f8d3ed8901a1
bbungsang <821heejin@naver.com>
2018-01-06 12:04:06 +0900

    permission, authentication 부분 주석 처리

--- django_app/config/settings.py
+++ django_app/config/settings.py
@@ -136,16 +136,16 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 # Django REST Framework 설정
-REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
-    ),
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.BasicAuthentication',
-    ),
-}
+# REST_FRAMEWORK = {
+#     'DEFAULT_PERMISSION_CLASSES': (
+#         'rest_framework.permissions.IsAuthenticated',
+#     ),
+#     'DEFAULT_AUTHENTICATION_CLASSES': (
+#         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+#         'rest_framework.authentication.SessionAuthentication',
+#         'rest_framework.authentication.BasicAuthentication',
+#     ),
+# }
 
 # CORS 설정
 CORS_ALLOW_CREDENTIALS = True
--- django_app/rent/views/scheduler.py
+++ django_app/rent/views/scheduler.py
@@ -6,9 +6,7 @@ from rest_framework.response import Response
 
 class CalendarView(APIView):
     def get(self, request, year, month):
-        print(year, month)
         cal = calendar.TextCalendar()
-        print(year, month)
         days = [0] + list(cal.itermonthdays(int(year), int(month)))
         week_list = [days[i:i+7] for i in range(0, len(days), 7)]
         if week_list[0][6] == 0:
