d7cb698ad5003ec9c5dda7a44129fdf03b96696f
Gihong Kim <gyong1211@gmail.com>
2018-01-07 01:18:42 +0900

    Add - UserListCreateView 생성 및 Serializer 추가

--- django_app/member/urls.py
+++ django_app/member/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import url
 
+from . import views
 
 urlpatterns = [
+    url(r'^$', views.UserListCreateView.as_view(), name='list_create'),
 ]
--- django_app/member/views.py
+++ django_app/member/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
-
+from rest_framework import generics
 # Create your views here.
+from rest_framework.filters import DjangoFilterBackend
+from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.permissions import IsAdminUser
+
+from .models import User
+from .serializers import SimpleUserSerializer, UserCreateSerializer
+
+
+class UserListCreateView(generics.ListCreateAPIView):
+    queryset = User.objects.all()
+    pagination_class = LimitOffsetPagination
+    filter_backends = (DjangoFilterBackend,)
+
+    def get_serializer_class(self):
+        if self.request.method == "GET":
+            return SimpleUserSerializer
+        elif self.request.method == "POST":
+            return UserCreateSerializer
+
+    def get_permissions(self):
+        if self.request.method == "GET":
+            return [IsAdminUser(), ]
+        else:
+            return []
