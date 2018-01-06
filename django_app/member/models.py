1d47e3e371f598bc645f9c31f0885909424065b1
Gihong Kim <gyong1211@gmail.com>
2018-01-07 00:38:45 +0900

    Fix - User model 오류사항 수정

--- django_app/member/models.py
+++ django_app/member/models.py
@@ -31,8 +31,8 @@ class UserManager(BaseUserManager):
             raise ValueError('Superuser must have is_staff=True.')
         if extra_fields.get('is_superuser') is not True:
             raise ValueError('Superuser must have is_superuser=True.')
-        if extra_fields.get('is_activate') is not True:
-            raise ValueError('Superuser must have is_activate=True.')
+        if extra_fields.get('is_active') is not True:
+            raise ValueError('Superuser must have is_active=True.')
         return self._create_user(email, password, **extra_fields)
 
 
